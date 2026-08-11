30391fc44c563621f460a51e3c22227209e84e88
Fernando Augusto de Oliveira Peralta <91924684+FernandoPeralta199x@users.noreply.github.com>
2026-08-11 12:46:35 -0300

    feat: add graphics director

---
+++ Assets/_Project/Scripts/Presentation/Rendering/TW08GraphicsDirector.cs
@@ -0,0 +1,77 @@
+using System;
+using UnityEngine;
+
+namespace TW08.Presentation
+{
+    [DefaultExecutionOrder(-850)]
+    [DisallowMultipleComponent]
+    public sealed class TW08GraphicsDirector : MonoBehaviour
+    {
+        [SerializeField] private TW08GraphicsProfile profile;
+        [SerializeField] private bool persistAcrossScenes = true;
+
+        public static TW08GraphicsDirector Instance { get; private set; }
+        public TW08GraphicsProfile Profile => profile;
+
+        public event Action<TW08GraphicsProfile> ProfileApplied;
+        public event Action<float, float> CameraShakeRequested;
+
+        public void Configure(TW08GraphicsProfile graphicsProfile, bool persist = true)
+        {
+            profile = graphicsProfile;
+            persistAcrossScenes = persist;
+            ApplyProfile();
+#if UNITY_EDITOR
+            if (!Application.isPlaying) UnityEditor.EditorUtility.SetDirty(this);
+#endif
+        }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            if (persistAcrossScenes)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+
+            ApplyProfile();
+        }
+
+        public void ApplyProfile()
+        {
+            if (profile == null)
+            {
+                return;
+            }
+
+            QualitySettings.vSyncCount = profile.VSyncCount;
+            QualitySettings.antiAliasing = profile.AntiAliasing;
+            Application.targetFrameRate = profile.TargetFrameRate;
+            ProfileApplied?.Invoke(profile);
+        }
+
+        public void RequestCameraShake(float amplitude, float duration)
+        {
+            if (amplitude <= 0f || duration <= 0f)
+            {
+                return;
+            }
+
+            CameraShakeRequested?.Invoke(amplitude, duration);
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+    }
+}
