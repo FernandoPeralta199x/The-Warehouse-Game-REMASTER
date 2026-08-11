48e04607f802060763e1516d52e9b52c97a06a7b
Fernando Augusto de Oliveira Peralta <91924684+FernandoPeralta199x@users.noreply.github.com>
2026-08-11 13:16:14 -0300

    fix: register dynamically configured racers

--- Assets/_Project/Scripts/Forklift/RacerProgress.cs
+++ Assets/_Project/Scripts/Forklift/RacerProgress.cs
@@ -16,18 +16,40 @@ namespace TW08.Race
 
         private void OnEnable()
         {
-            raceManager?.Register(this);
+            if (raceManager != null)
+            {
+                raceManager.Register(this);
+            }
         }
 
         private void OnDisable()
         {
-            raceManager?.Unregister(this);
+            if (raceManager != null)
+            {
+                raceManager.Unregister(this);
+            }
         }
 
         public void Configure(RaceManager manager, string id)
         {
+            if (isActiveAndEnabled && raceManager != null && raceManager != manager)
+            {
+                raceManager.Unregister(this);
+            }
+
             raceManager = manager;
-            racerId = id;
+            racerId = string.IsNullOrWhiteSpace(id) ? gameObject.name : id;
+
+            // AddComponent invokes OnEnable before editor/runtime builders can call Configure.
+            // Register here as well so dynamically authored racers always enter standings.
+            if (isActiveAndEnabled && raceManager != null)
+            {
+                raceManager.Register(this);
+            }
+
+#if UNITY_EDITOR
+            if (!Application.isPlaying) UnityEditor.EditorUtility.SetDirty(this);
+#endif
         }
 
         public void ResetProgress()
